52665806453a628a0bf55be5250790a2949066b2
itamarLeiboMoveo <itamarl@moveo.co.il>
2024-06-05 14:13:21 +0300

    dropdown filter and small adjustments

---
+++ src/pages/HomePage/components/SearchBar/DropDown.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { typesDict } from '../../../../services/TypesDict.tsx';
+import './DropDown.scss';
+
+const DropDown = ({ setDropDown, value }) => {
+  return (
+    <select value={value} onChange={setDropDown}>
+        <option value="" >Insert a type</option>
+      {Object.entries(typesDict).map(([key, label]) => (
+        <option key={key} value={key}>
+          {key}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default DropDown;
--- src/pages/HomePage/components/SearchBar/SearchBar.tsx
+++ src/pages/HomePage/components/SearchBar/SearchBar.tsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import fetchPokemons, { getPokemonsDict, getPokemonsList } from '../../../../services/FetchService.tsx';
 import './SearchBar.scss';
+import DropDown from './DropDown.tsx';
 
 function SearchBar({ setFilteredPokemons }) {
     const [pokeArr, setPokeArr] = useState<pokemon[]>([]);
     const [pokeName, setPokeName] = useState('');
+    const [selectedType, setSelectedType] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -22,17 +24,33 @@ function SearchBar({ setFilteredPokemons }) {
             if (pokeName === '') {
                 setFilteredPokemons(pokeArr);
             } else {
-                const filtered = pokeArr.filter(poke => poke.name.toLowerCase().includes(pokeName.toLowerCase()));
+                const filteredPrefix = pokeArr.filter(poke => poke.name.toLowerCase().startsWith(pokeName.toLowerCase()));
+                const filtered = filteredPrefix.concat(pokeArr.filter(poke => 
+                    poke.name.toLowerCase().includes(pokeName.toLowerCase()) &&
+                    !filteredPrefix.includes(poke)));
                 setFilteredPokemons(filtered);
             }
         };
         filterPokemons();
     }, [pokeName, pokeArr, setFilteredPokemons]);
 
+    useEffect(() => {
+        const filterTypes = () => {
+            if(selectedType === ''){
+                setFilteredPokemons(pokeArr);
+            }
+            else{
+                const filtered = pokeArr.filter(poke => poke.types.includes(selectedType));
+                setFilteredPokemons(filtered);
+            }
+        };
+        filterTypes();
+    }, [selectedType, pokeArr, setFilteredPokemons]);
+
     function handleSubmit(event) {
         event.preventDefault();
 
-        const matchedPoke = pokeArr.find((poke) => poke.name === pokeName);
+        const matchedPoke = pokeArr.find((poke) => poke.name === pokeName.toLowerCase());
         if (matchedPoke) {
             navigate('/internal-page', { state: matchedPoke });
         } else {
@@ -42,17 +60,19 @@ function SearchBar({ setFilteredPokemons }) {
 
     function handleInput(event) {
         setPokeName(event.target.value);
+    }
 
+
+    function handleDropDown(event){
+        setSelectedType(event.target.value);
     }
 
     return (
         <div >
             <form className='search-container' onSubmit={handleSubmit}>
                 <input value={pokeName} type='text' onChange={handleInput} />
-                {/* <div class="dropdown-content">
-                    <button >Link 1</button>
-                </div> */}
                 <button >Search</button>
+                <DropDown value={selectedType} setDropDown={handleDropDown}/>
             </form>
         </div>
     );
